b36546b48da55bc57518278ea2289e292f81f24a
Garv Jain <84724429+garv208@users.noreply.github.com>
2023-01-18 20:51:44 +0530

    Update Counter.v

--- Counter.v
+++ Counter.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03.01.2023 20:38:45
-// Design Name: 
-// Module Name: Counter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module Counter(input clk, rst,
     output reg counter_out);
     reg [15:0]counter;
